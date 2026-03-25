39dc5f12cf7d9872277df41ed39c57960f2fd91a
yhjiang504 <yhjiang504@gmail.com>
2026-03-25 09:53:30 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'market-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'icon.png'
+];
+
+// 安裝 Service Worker 並快取檔案
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// 攔截請求，優先從快取讀取 (離線支持)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
